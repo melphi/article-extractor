59a027fd6b76b6851d90a3342f7a9cff06a7dd1d
robertom <my@email.com>
2017-06-29 11:13:55 +0200

    Deployment

--- python/app/async.py
+++ python/app/async.py
@@ -6,6 +6,6 @@ from uvloop import new_event_loop
 # TODO: Consider Semaphores
 # https://pawelmhm.github.io/asyncio/python/aiohttp/2016/04/22/asyncio-aiohttp.html
 
-client_session = ClientSession()
 event_loop = new_event_loop()
 asyncio.set_event_loop(event_loop)
+client_session = ClientSession(loop=event_loop)
--- python/extract_server.py
+++ python/extract_server.py
@@ -5,4 +5,5 @@ from aiohttp import web
 from app.app import app
 
 
+print('starting service at 127.0.0.1:8000')
 web.run_app(app, host='127.0.0.1', port=8000)
